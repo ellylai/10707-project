17929736f2ecd8d077bfc25a5ba1255480275e3b
ellylai <ellysel@andrew.cmu.edu>
2026-03-19 08:04:09 -0400

    Update training_utils.py

--- utils/training_utils.py
+++ utils/training_utils.py
@@ -18,13 +18,22 @@ def train_epoch(model, loader, optimizer, criterion, device):
 
 def validate(model, loader, criterion, device):
     model.eval()
+    total_loss = 0
     correct = 0
     total = 0
+    
     with torch.no_grad():
         for waveforms, labels in tqdm(loader, desc="Validating"):
             waveforms, labels = waveforms.to(device), labels.to(device)
             _, logits = model(waveforms)
+            
+            loss = criterion(logits, labels)
+            total_loss += loss.item()
+            
             preds = torch.argmax(logits, dim=1)
             correct += (preds == labels).sum().item()
             total += labels.size(0)
-    return total_loss / len(loader), correct / total
+    avg_loss = total_loss / len(loader)
+    avg_acc = correct / total
+    
+    return avg_loss, avg_acc
