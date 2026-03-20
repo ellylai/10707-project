a94ea9d273f782231b8b6403b3a924d05f0e7e4d
ellylai <ellysel@andrew.cmu.edu>
2026-03-20 10:11:21 -0400

    Update dualstream.py

--- model/dualstream.py
+++ model/dualstream.py
@@ -1,4 +1,21 @@
-# dual-stream audio deepfake detection model
+'''
+DUALSTREAM: an audio deepfake detection model
+
+Prosodic features (4) = fundamental frequency F_0, jitter, shimmer, harmonicity (HNR)
+Digital features (embeddings) = wav2vec2 replaces mel-spectrogram
+
+Fusion mechanism = gated multimodal unit (GMU) / cross-attention / cross-attention pooling
+
+Input = .wav file
+
+[digistream]                                            [biostream] 
+Wav2vec2-XLSR → embedding                               Feature Extractor → prosodic features
+Embedding → Convolutional Transformer → output          Prosodic features → LSTM → output
+
+Prosodic output + Digital output → Fusion mechanism
+Fusion mechanism → sigmoid → binary classification
+Output = binary classification
+'''
 
 class DUALSTREAM(nn.Module):
     """
