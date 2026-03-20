71ac4e9063cd0c093d138c5bf10cf3185e0efd6c
ellylai <ellysel@andrew.cmu.edu>
2026-03-19 20:59:06 -0400

    lets get started zzzzz

---
+++ model/dualstream.py
@@ -0,0 +1,12 @@
+# dual-stream audio deepfake detection model
+
+class DUALSTREAM(nn.Module):
+    """
+    Input: raw 1D .wav waveform
+    Output: binary classification
+    """
+    def __init__(self, args):
+        ...
+    
+    def forward(self, x):
+        ...
